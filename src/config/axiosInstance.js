f245d5c865f6e883b145ce66aec57d7e3b0d4e3e
shahil-456 <shahilshadh037@gmail.com>
2025-02-24 12:57:14 +0530

    Update axiosInstance.js

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: `https://job-board-platform-server.vercel.app/api`,
     withCredentials: true,
+    headers: {
+    'Content-Type': 'application/json',
+  },
 });
